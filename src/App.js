4d7997bebd8d34be76bc032920b170aa96a85965
Lari Vaartio <lari@layer.fi>
2020-01-07 18:24:32 +0200

    Add exercise 5.1

--- src/App.js
+++ src/App.js
@@ -1,22 +1,111 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+
+import login from './services/login' 
+import blogService from './services/blogs'
+import Notification from './components/Notification'
+import Blog from './components/Blog'
+
 //import logo from './logo.svg';
 //import './App.css';
 
 function App() {
+
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [user, setUser] = useState(null)
+  const [message, setMessage] = useState(null)
+  const [blogs, setBlogs] = useState([])
+
+  useEffect(() => {
+    const fetchBlogs = async () => {
+      const initialBlogs = await blogService.getAll()
+      setBlogs(initialBlogs)
+    }
+    fetchBlogs()
+  }, [])
+
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem('loggedInUser')
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON)
+      setUser(user)
+      blogService.setToken(user.token)
+    }
+  }, [])
+
+  const setNotification = (message, type = 'info') => {
+    setMessage({
+      message: message,
+      type: type,
+    })
+    setTimeout(() => {
+      setMessage(null)
+    }, 5000)
+  }
+
+  const handleLogin = async (event) => {
+    event.preventDefault()
+    try {
+      const loggedInUser = await login({ username, password })
+      window.localStorage.setItem('loggedInUser', JSON.stringify(loggedInUser))
+      setUser(loggedInUser)
+      setUsername('')
+      setPassword('')
+    }
+    catch (exception) {
+      setNotification(`login failed`, 'error')
+    }
+  }
+
+  const BlogsView = (props) => {
+    const userBlogs = props.blogs.filter(blog => blog.user.username === props.user.username)
+    return (
+      <div>
+        <h2>blogs</h2>
+        <p>{user.name} logged in</p>
+        {userBlogs.map(blog =>
+          <Blog key={blog.id} blog={blog} />
+        )}
+      </div>
+    )
+  }
+
+  const LoginView = (props) => {
+    return (
+      <>
+      <h2>Log to application</h2>
+      <form onSubmit={handleLogin}>
+        <div>
+          username
+            <input
+            type="text"
+            value={username}
+            name="Username"
+            onChange={({ target }) => setUsername(target.value)}
+          />
+        </div>
+        <div>
+          password
+            <input
+            type="password"
+            value={password}
+            name="Password"
+            onChange={({ target }) => setPassword(target.value)}
+          />
+        </div>
+        <button type="submit">login</button>
+      </form>
+      </>
+    )
+  }
+
   return (
     <div className="App">
+      <Notification message={message} />
       <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      {
+        user === null ? <LoginView /> : <BlogsView blogs={blogs} user={user} />
+      }
       </header>
     </div>
   );
---
+++ src/components/Notification.js
@@ -0,0 +1,42 @@
+import React from 'react'
+
+const getStyle = type => {
+    if (type === 'error') {
+        return {
+            color: 'red',
+            background: 'lightgrey',
+            fontSize: 20,
+            borderStyle: 'solid',
+            borderRadius: 5,
+            padding: 10,
+            marginBottom: 10,
+        }
+    }
+    else {
+        return {
+            color: 'green',
+            background: 'lightgrey',
+            fontSize: 20,
+            borderStyle: 'solid',
+            borderRadius: 5,
+            padding: 10,
+            marginBottom: 10,
+        }
+    }
+}
+
+const Notification = ({ message }) => {
+    if (message === null) {
+        return null
+    }
+
+    const style = getStyle(message.type)
+
+    return (
+        <div style={style}>
+        {message.message}
+        </div>
+    )
+}
+
+export default Notification
